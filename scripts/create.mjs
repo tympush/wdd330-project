55458ce7ed86af563f5954e7116a5db3b1c11716
tympush <tymur.pushnoy@gmail.com>
2025-06-03 14:24:57 +0200

    added functionality for the reload buttons

--- scripts/create.mjs
+++ scripts/create.mjs
@@ -1,9 +1,19 @@
 import { renderColorBar, renderEmojiBar } from "./dynamic-elements.mjs";
 
-const colorBar = document.querySelector('.colorBar');
-const emojiBar = document.querySelector('.emojiBar');
-
 document.addEventListener("DOMContentLoaded", () => {
+    const colorBar = document.querySelector('.colorBar');
+    const emojiBar = document.querySelector('.emojiBar');
+    const colorReloadButton = document.getElementById('colorReloadButton');
+    const emojiReloadButton = document.getElementById('emojiReloadButton');
+
     renderColorBar(colorBar);
     renderEmojiBar(emojiBar);
+
+    colorReloadButton.addEventListener('click', () => {
+        renderColorBar(colorBar);
+    });
+
+    emojiReloadButton.addEventListener('click', () => {
+        renderEmojiBar(emojiBar);
+    });
 });
